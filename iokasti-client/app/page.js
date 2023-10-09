b3c68aca4e7ef770bdf7e11df290220cf6f13faa
skrvasilis <skrvasilis@gmail.com>
2023-10-09 09:37:20 +0200

    fixed some issues

--- iokasti-client/app/page.js
+++ iokasti-client/app/page.js
@@ -1,11 +1,10 @@
-
 export default function Home() {
-
   return (
-    <main>
-      Home
+    <main className="bg-orange-800">
+    <h1 className="p-4">Home</h1>
+      
       {/* it doesn't autoplay on refresh? */}
-      <div style={{ padding: "56.25% 0 0 0", position: "relative" }}>
+      {/* <div style={{ padding: "56.25% 0 0 0", position: "relative" }}>
         <iframe
           src="https://player.vimeo.com/video/368412716?h=9e28e76ddd&autoplay=1&loop=1"
           style={{
@@ -15,29 +14,27 @@ export default function Home() {
             width: "100%",
             height: "100%",
           }}
-          frameborder="0"
+          frameBorder="0"
           allow="autoplay; fullscreen; picture-in-picture"
-          allowfullscreen
+          allowFullScreen
         ></iframe>
-      </div>
-
+      </div> */}
       <div style={{ padding: "56.25% 0 0 0", position: "relative" }}>
-      <video style={{
+        <video
+          autoPlay
+          loop
+          style={{
             position: "absolute",
             top: 0,
             left: "0",
             width: "100%",
             height: "100%",
-          }}>
-        <source src='../public/cat.webm' />
-      </video>
-          </div>
-
-
+          }}
+        >
+          <source src="/cat.webm" />
+        </video>
+      </div>
       <script src="https://player.vimeo.com/api/player.js"></script>
     </main>
   );
 }
-
-
-
--- iokasti-client/tailwind.config.js
+++ iokasti-client/tailwind.config.js
@@ -1,17 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-   './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,jsx}',
-  ],
+  content: ["./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,jsx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
