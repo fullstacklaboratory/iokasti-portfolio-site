1283bb5c8d924a0c15a706407ee9088e0ab3cc79
mrcll <passes@protonmail.com>
2023-10-12 00:05:20 +0200

    video fixed, nav and other things changed

--- iokasti-client/app/page.js
+++ iokasti-client/app/page.js
@@ -1,15 +1,15 @@
 export default function Home() {
   return (
-    <main className="bg-orange-800">
-      <h1 className="p-4 text-2xl">Home</h1>
+    <>
+      <h1 className="p-4 text-2xl">Who's Iokasti?</h1>
 
       {/* it doesn't autoplay on refresh? */}
       {/* <div style={{ padding: "56.25% 0 0 0", position: "relative" }}>
         <iframe
-          src="https://player.vimeo.com/video/368412716?h=9e28e76ddd&autoplay=1&loop=1"
+          src="https://player.vimeo.com/video/368412716?h=9e28e76ddd&autoplay=1&loop=1&autopause=0&muted=1&background=1"
           style={{
             position: "absolute",
-            top: 0,
+            top: "0",
             left: "0",
             width: "100%",
             height: "100%",
@@ -17,27 +17,40 @@ export default function Home() {
           frameBorder="0"
           allow="autoplay; fullscreen; picture-in-picture"
           allowFullScreen
-        ></iframe>
+        ></iframe> */}
+      {/* <iframe title="vimeo-player" src="https://player.vimeo.com/video/872349418?h=8220cf9817&autoplay=1&loop=1&autopause=0&muted=1&background=1&quality=1080p"      
+                  style={{
+                    position: "absolute",
+                    top: "0",
+                    left: "0",
+                    width: "100%",
+                    height: "100%",
+                  }} frameborder="0"    allowfullscreen></iframe>
       </div> */}
 
       {/* it doesn't autoplay on refresh? */}
 
-      <div style={{ padding: "56.25% 0 0 0", position: "relative" }}>
-        <video
-          autoPlay
-          loop
-          style={{
-            position: "absolute",
-            top: 0,
-            left: "0",
-            width: "100%",
-            height: "100%",
-          }}
-        >
-          <source src="/cat.webm" />
-        </video>
-      </div>
-      <script src="https://player.vimeo.com/api/player.js"></script>
-    </main>
+      <video
+        autoPlay
+        loop
+        muted
+        style={{
+          position: "absolute",
+          top: "0",
+          left: "0",
+          right: "0",
+          width: "100%",
+          height: "100%",
+          "object-fit": "cover",
+          "z-index": "-1",
+        }}
+      >
+        <source src="/iokasti-home.webm" type="video/webm" />
+        Your browser does not support the video tag. Please use a modern
+        browser.
+      </video>
+
+      {/* <script src="https://player.vimeo.com/api/player.js"></script> */}
+    </>
   );
 }
