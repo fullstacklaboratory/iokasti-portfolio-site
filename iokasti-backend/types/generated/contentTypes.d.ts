d9a359c07899cbf76196415970c11df73b4ae5a8
skrvasilis <skrvasilis@gmail.com>
2023-10-20 15:54:57 +0200

    commit

--- iokasti-backend/types/generated/contentTypes.d.ts
+++ iokasti-backend/types/generated/contentTypes.d.ts
@@ -709,6 +709,40 @@ export interface ApiAboutAbout extends Schema.SingleType {
   };
 }
 
+export interface ApiLandPageLandPage extends Schema.SingleType {
+  collectionName: 'land_pages';
+  info: {
+    singularName: 'land-page';
+    pluralName: 'land-pages';
+    displayName: 'landPage';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    vimeo_link: Attribute.String;
+    Instagram_Link: Attribute.String;
+    Email: Attribute.Email;
+    Video: Attribute.Media & Attribute.Required;
+    Name: Attribute.String;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::land-page.land-page',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::land-page.land-page',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface ApiProjectProject extends Schema.CollectionType {
   collectionName: 'projects';
   info: {
@@ -764,6 +798,7 @@ declare module '@strapi/strapi' {
       'plugin::users-permissions.user': PluginUsersPermissionsUser;
       'plugin::i18n.locale': PluginI18NLocale;
       'api::about.about': ApiAboutAbout;
+      'api::land-page.land-page': ApiLandPageLandPage;
       'api::project.project': ApiProjectProject;
     }
   }
