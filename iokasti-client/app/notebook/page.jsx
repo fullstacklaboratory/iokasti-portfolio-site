fdb96e4a0ee8354f196b2c583a5404ac6055f3c5
skrvasilis <skrvasilis@gmail.com>
2023-12-02 14:26:24 +0100

    notebook ready

--- iokasti-client/app/notebook/page.jsx
+++ iokasti-client/app/notebook/page.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import Instagram from "@/components/Instagram";
+import { getNotebook } from "@/lib/notebook";
 
-const Notebook = () => {
-  return <Instagram />;
+const Notebook = async () => {
+  const data = await getNotebook();
+  const images = data.images.map((item) => {
+    {
+      return item.attributes.url;
+    }
+  });
+
+  return <Instagram images={images} />;
 };
 
 export default Notebook;
