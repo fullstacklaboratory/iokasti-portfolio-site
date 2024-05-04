4a87104902a1a7f886766a9b08c544d0dc703bf1
mrcll <passes@protonmail.com>
2024-05-04 20:29:25 +0200

    cloned to server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,14 @@ module.exports = {
       name: "iokasti-backend",
       script: "npm",
       args: "start",
-      cwd: "/var/www/iokasti/iokasti-backend",
+      cwd: "~/websites/iokasti/iokasti-backend",
       instances: "max",
       exec_mode: "cluster"
     }, {
       name: "iokasti-frontend",
       script: "npm",
       args: "start",
-      cwd: "/var/www/iokasti/iokasti-client",
+      cwd: "~/websites/iokasti/iokasti-client",
       instances: "max",
       exec_mode: "cluster"
     }]
