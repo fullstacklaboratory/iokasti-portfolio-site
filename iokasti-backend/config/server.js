5dbf662d9a8d40c1b3989a977a15e9fc00322f75
mrcll <passes@protonmail.com>
2024-05-05 23:15:51 +0200

    deleted install

--- iokasti-backend/config/server.js
+++ iokasti-backend/config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: 'https://api.iokastimantzog.com',
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
   },
