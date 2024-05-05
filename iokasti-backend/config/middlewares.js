2b77e2d0d6a9637ffcec30d56b9a607b89877c05
mrcll <passes@protonmail.com>
2024-05-05 23:01:22 +0200

    deleted install

--- iokasti-backend/config/middlewares.js
+++ iokasti-backend/config/middlewares.js
@@ -12,7 +12,7 @@ module.exports = [
   {
     name: "strapi::cors",
     config: {
-      origin: ["http://localhost:3000", "https://iokastimantzog.com"],
+      origin: ["http://localhost:3000", "http://localhost:1337", "https://iokastimantzog.com", "https://api.iokastimantzog.com"],
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
       headers: ["Content-Type", "Authorization", "Origin", "Accept"],
       keepHeaderOnError: true,
