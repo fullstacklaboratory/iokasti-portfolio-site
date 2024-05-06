4cde2151d776c35a189a8f8f4a271bbe242fc726
mrcll <passes@protonmail.com>
2024-05-06 19:21:32 +0200

    ecosystem change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,23 @@
 module.exports = {
-    apps : [{
-      name: "iokasti-backend",
-      cwd: "~/websites/iokasti/iokasti-backend",
-      script: "npm",
-      args: "start",
-      instances: "max",
-      exec_mode: "cluster"
-    }, {
-      name: "iokasti-frontend",
-      cwd: "~/websites/iokasti/iokasti-client",
-      script: "npm",
-      args: "start",
-      instances: "max",
-      exec_mode: "cluster"
-    }]
-  }
+  apps : [{
+    name: "iokasti-backend",
+    cwd: "~/websites/iokasti/iokasti-backend",
+    script: "npm",
+    args: "start",
+    instances: "max",
+    exec_mode: "cluster",
+    env: {
+      NODE_ENV: "production"
+    }
+  }, {
+    name: "iokasti-frontend",
+    cwd: "~/websites/iokasti/iokasti-client",
+    script: "npm",
+    args: "start",
+    instances: "max",
+    exec_mode: "cluster",
+    env: {
+      NODE_ENV: "production"
+    }
+  }]
+}
