2dba8c556e8f48c0515b04daa0a8c2fdbafff609
mrcll <passes@protonmail.com>
2024-05-06 18:39:27 +0200

    server setup

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
     apps : [{
       name: "iokasti-backend",
+      cwd: "~/websites/iokasti/iokasti-backend",
       script: "npm",
       args: "start",
-      cwd: "~/websites/iokasti/iokasti-backend",
       instances: "max",
       exec_mode: "cluster"
     }, {
       name: "iokasti-frontend",
+      cwd: "~/websites/iokasti/iokasti-client",
       script: "npm",
       args: "start",
-      cwd: "~/websites/iokasti/iokasti-client",
       instances: "max",
       exec_mode: "cluster"
     }]
