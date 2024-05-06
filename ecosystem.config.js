cb2f453ef0832fa819a9bc05695c8e0240a23f40
mrcll <passes@protonmail.com>
2024-05-06 19:46:16 +0200

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  apps : [{
-    name: "iokasti-backend",
-    cwd: "~/websites/iokasti/iokasti-backend",
-    script: "npm",
-    args: "start",
-    instances: "max",
-    exec_mode: "cluster",
-    env: {
-      NODE_ENV: "production"
-    }
-  }, {
-    name: "iokasti-frontend",
-    cwd: "~/websites/iokasti/iokasti-client",
-    script: "npm",
-    args: "start",
-    instances: "max",
-    exec_mode: "cluster",
-    env: {
-      NODE_ENV: "production"
-    }
-  }]
-}
+  apps: [
+    {
+      name: "iokasti-backend",
+      cwd: "/home/mrcll/websites/iokasti/iokasti-backend",
+      script: "npm",
+      args: "start",
+      instances: "max",
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "iokasti-frontend",
+      cwd: "/home/mrcll/websites/iokasti/iokasti-client",
+      script: "npm",
+      args: "start",
+      instances: "max",
+      exec_mode: "cluster",
+    },
+  ],
+};
